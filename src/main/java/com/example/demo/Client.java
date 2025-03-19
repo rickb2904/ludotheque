4aae27776de67f9ce5ff5fcbca7e4fcfa2e44b1d
rickbouyaghi <rickos2003@gmail.com>
2025-03-19 16:55:51 +0100

    LOMBOK

--- src/main/java/com/example/demo/Client.java
+++ src/main/java/com/example/demo/Client.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 import jakarta.persistence.*;
 
+import lombok.*;
+
 @Entity
+@Table(name = "client")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
 
     @Id
@@ -12,56 +18,4 @@ public class Client {
     private String prenom;
     private String email;
     private String noTelephone;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "adresse_id")
-    private Adresse adresse;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNoTelephone() {
-        return noTelephone;
-    }
-
-    public void setNoTelephone(String noTelephone) {
-        this.noTelephone = noTelephone;
-    }
-
-    public Adresse getAdresse() {
-        return adresse;
-    }
-
-    public void setAdresse(Adresse adresse) {
-        this.adresse = adresse;
-    }
 }
